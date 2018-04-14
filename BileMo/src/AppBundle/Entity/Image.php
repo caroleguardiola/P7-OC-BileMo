d4955a4af407e3b9de88a259ebb4e2e12ca8c690
caroleguardiola <carole.guardiola@gmail.com>
2018-04-14 17:15:35 +0200

    Created the entity Image with repository.

---
+++ BileMo/src/AppBundle/Entity/Image.php
@@ -0,0 +1,189 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Image
+ *
+ * @ORM\Table(name="image")
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\ImageRepository")
+ */
+class Image
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer", options={"unsigned":true})
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="extension", type="string", length=45)
+     */
+    private $extension;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="alt", type="string", length=255)
+     */
+    private $alt;
+
+    /**
+     * @var datetime_immutable
+     *
+     * @ORM\Column(name="date_creation", type="datetime_immutable")
+     */
+    private $dateCreation;
+
+    /**
+     * @var datetime_immutable|null
+     *
+     * @ORM\Column(name="updated_at", type="datetime_immutable", nullable=true)
+     */
+    private $updatedAt;
+
+    /**
+     * @var datetime_immutable|null
+     *
+     * @ORM\Column(name="date_deactivation", type="datetime_immutable", nullable=true)
+     */
+    private $dateDeactivation;
+
+
+    /**
+     * Get id.
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set extension.
+     *
+     * @param string $extension
+     *
+     * @return Image
+     */
+    public function setExtension($extension)
+    {
+        $this->extension = $extension;
+
+        return $this;
+    }
+
+    /**
+     * Get extension.
+     *
+     * @return string
+     */
+    public function getExtension()
+    {
+        return $this->extension;
+    }
+
+    /**
+     * Set alt.
+     *
+     * @param string $alt
+     *
+     * @return Image
+     */
+    public function setAlt($alt)
+    {
+        $this->alt = $alt;
+
+        return $this;
+    }
+
+    /**
+     * Get alt.
+     *
+     * @return string
+     */
+    public function getAlt()
+    {
+        return $this->alt;
+    }
+
+    /**
+     * Set dateCreation.
+     *
+     * @param datetime_immutable $dateCreation
+     *
+     * @return Image
+     */
+    public function setDateCreation($dateCreation)
+    {
+        $this->dateCreation = $dateCreation;
+
+        return $this;
+    }
+
+    /**
+     * Get dateCreation.
+     *
+     * @return datetime_immutable
+     */
+    public function getDateCreation()
+    {
+        return $this->dateCreation;
+    }
+
+    /**
+     * Set updatedAt.
+     *
+     * @param datetime_immutable|null $updatedAt
+     *
+     * @return Image
+     */
+    public function setUpdatedAt($updatedAt = null)
+    {
+        $this->updatedAt = $updatedAt;
+
+        return $this;
+    }
+
+    /**
+     * Get updatedAt.
+     *
+     * @return datetime_immutable|null
+     */
+    public function getUpdatedAt()
+    {
+        return $this->updatedAt;
+    }
+
+    /**
+     * Set dateDeactivation.
+     *
+     * @param datetime_immutable|null $dateDeactivation
+     *
+     * @return Image
+     */
+    public function setDateDeactivation($dateDeactivation = null)
+    {
+        $this->dateDeactivation = $dateDeactivation;
+
+        return $this;
+    }
+
+    /**
+     * Get dateDeactivation.
+     *
+     * @return datetime_immutable|null
+     */
+    public function getDateDeactivation()
+    {
+        return $this->dateDeactivation;
+    }
+}
