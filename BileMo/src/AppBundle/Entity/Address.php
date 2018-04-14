5e692e91347957fa989fa8b1d11318f4f1133727
caroleguardiola <carole.guardiola@gmail.com>
2018-04-14 17:32:24 +0200

    Created the entity Address with repository.

---
+++ BileMo/src/AppBundle/Entity/Address.php
@@ -0,0 +1,282 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Address
+ *
+ * @ORM\Table(name="address")
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\AddressRepository")
+ */
+class Address
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer", options={"unsigned":true})
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="recipient", type="string", length=255, unique=true)
+     */
+    private $recipient;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="street_address", type="string", length=255)
+     */
+    private $streetAddress;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="zip_code", type="string", length=5)
+     */
+    private $zipCode;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="city", type="string", length=255)
+     */
+    private $city;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="country", type="string", length=255, unique=true)
+     */
+    private $country;
+
+    /**
+     * @var datetime_immutable
+     *
+     * @ORM\Column(name="date_creation", type="datetime_immutable")
+     */
+    private $dateCreation;
+
+    /**
+     * @var datetime_immutable|null
+     *
+     * @ORM\Column(name="updated_at", type="datetime_immutable", nullable=true)
+     */
+    private $updatedAt;
+
+    /**
+     * @var datetime_immutable|null
+     *
+     * @ORM\Column(name="date_deactivation", type="datetime_immutable", nullable=true)
+     */
+    private $dateDeactivation;
+
+
+    /**
+     * Get id.
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set recipient.
+     *
+     * @param string $recipient
+     *
+     * @return Address
+     */
+    public function setRecipient($recipient)
+    {
+        $this->recipient = $recipient;
+
+        return $this;
+    }
+
+    /**
+     * Get recipient.
+     *
+     * @return string
+     */
+    public function getRecipient()
+    {
+        return $this->recipient;
+    }
+
+    /**
+     * Set streetAddress.
+     *
+     * @param string $streetAddress
+     *
+     * @return Address
+     */
+    public function setStreetAddress($streetAddress)
+    {
+        $this->streetAddress = $streetAddress;
+
+        return $this;
+    }
+
+    /**
+     * Get streetAddress.
+     *
+     * @return string
+     */
+    public function getStreetAddress()
+    {
+        return $this->streetAddress;
+    }
+
+    /**
+     * Set zipCode.
+     *
+     * @param string $zipCode
+     *
+     * @return Address
+     */
+    public function setZipCode($zipCode)
+    {
+        $this->zipCode = $zipCode;
+
+        return $this;
+    }
+
+    /**
+     * Get zipCode.
+     *
+     * @return string
+     */
+    public function getZipCode()
+    {
+        return $this->zipCode;
+    }
+
+    /**
+     * Set city.
+     *
+     * @param string $city
+     *
+     * @return Address
+     */
+    public function setCity($city)
+    {
+        $this->city = $city;
+
+        return $this;
+    }
+
+    /**
+     * Get city.
+     *
+     * @return string
+     */
+    public function getCity()
+    {
+        return $this->city;
+    }
+
+    /**
+     * Set country.
+     *
+     * @param string $country
+     *
+     * @return Address
+     */
+    public function setCountry($country)
+    {
+        $this->country = $country;
+
+        return $this;
+    }
+
+    /**
+     * Get country.
+     *
+     * @return string
+     */
+    public function getCountry()
+    {
+        return $this->country;
+    }
+
+    /**
+     * Set dateCreation.
+     *
+     * @param datetime_immutable $dateCreation
+     *
+     * @return Address
+     */
+    public function setDateCreation($dateCreation)
+    {
+        $this->dateCreation = $dateCreation;
+
+        return $this;
+    }
+
+    /**
+     * Get dateCreation.
+     *
+     * @return datetime_immutable
+     */
+    public function getDateCreation()
+    {
+        return $this->dateCreation;
+    }
+
+    /**
+     * Set updatedAt.
+     *
+     * @param datetime_immutable|null $updatedAt
+     *
+     * @return Address
+     */
+    public function setUpdatedAt($updatedAt = null)
+    {
+        $this->updatedAt = $updatedAt;
+
+        return $this;
+    }
+
+    /**
+     * Get updatedAt.
+     *
+     * @return datetime_immutable|null
+     */
+    public function getUpdatedAt()
+    {
+        return $this->updatedAt;
+    }
+
+    /**
+     * Set dateDeactivation.
+     *
+     * @param datetime_immutable|null $dateDeactivation
+     *
+     * @return Address
+     */
+    public function setDateDeactivation($dateDeactivation = null)
+    {
+        $this->dateDeactivation = $dateDeactivation;
+
+        return $this;
+    }
+
+    /**
+     * Get dateDeactivation.
+     *
+     * @return datetime_immutable|null
+     */
+    public function getDateDeactivation()
+    {
+        return $this->dateDeactivation;
+    }
+}
