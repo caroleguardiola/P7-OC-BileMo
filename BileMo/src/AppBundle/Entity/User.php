9d91d34b189e878afd851360de27a356b73dd86c
caroleguardiola <carole.guardiola@gmail.com>
2018-04-14 17:26:18 +0200

    Created the entity User with repository.

---
+++ BileMo/src/AppBundle/Entity/User.php
@@ -0,0 +1,313 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * User
+ *
+ * @ORM\Table(name="user")
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\UserRepository")
+ */
+class User
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer", options={"unsigned":true})
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="user_number", type="integer", unique=true, options={"unsigned":true})
+     */
+    private $userNumber;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="username", type="string", length=255, unique=true)
+     */
+    private $username;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="first_name", type="string", length=255)
+     */
+    private $firstName;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="last_name", type="string", length=255)
+     */
+    private $lastName;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="email", type="string", length=255, unique=true)
+     */
+    private $email;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="password", type="string", length=60, unique=true)
+     */
+    private $password;
+
+    /**
+     * @var datetime_immutable
+     *
+     * @ORM\Column(name="date_creation", type="datetime_immutable")
+     */
+    private $dateCreation;
+
+    /**
+     * @var datetime_immutable|null
+     *
+     * @ORM\Column(name="updatedat", type="datetime_immutable", nullable=true)
+     */
+    private $updatedAt;
+
+    /**
+     * @var datetime_immutable|null
+     *
+     * @ORM\Column(name="date_deactivation", type="datetime_immutable", nullable=true)
+     */
+    private $dateDeactivation;
+
+
+    /**
+     * Get id.
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set userNumber.
+     *
+     * @param int $userNumber
+     *
+     * @return User
+     */
+    public function setUserNumber($userNumber)
+    {
+        $this->userNumber = $userNumber;
+
+        return $this;
+    }
+
+    /**
+     * Get userNumber.
+     *
+     * @return int
+     */
+    public function getUserNumber()
+    {
+        return $this->userNumber;
+    }
+
+    /**
+     * Set username.
+     *
+     * @param string $username
+     *
+     * @return User
+     */
+    public function setUsername($username)
+    {
+        $this->username = $username;
+
+        return $this;
+    }
+
+    /**
+     * Get username.
+     *
+     * @return string
+     */
+    public function getUsername()
+    {
+        return $this->username;
+    }
+
+    /**
+     * Set firstName.
+     *
+     * @param string $firstName
+     *
+     * @return User
+     */
+    public function setFirstName($firstName)
+    {
+        $this->firstName = $firstName;
+
+        return $this;
+    }
+
+    /**
+     * Get firstName.
+     *
+     * @return string
+     */
+    public function getFirstName()
+    {
+        return $this->firstName;
+    }
+
+    /**
+     * Set lastName.
+     *
+     * @param string $lastName
+     *
+     * @return User
+     */
+    public function setLastName($lastName)
+    {
+        $this->lastName = $lastName;
+
+        return $this;
+    }
+
+    /**
+     * Get lastName.
+     *
+     * @return string
+     */
+    public function getLastName()
+    {
+        return $this->lastName;
+    }
+
+    /**
+     * Set email.
+     *
+     * @param string $email
+     *
+     * @return User
+     */
+    public function setEmail($email)
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    /**
+     * Get email.
+     *
+     * @return string
+     */
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    /**
+     * Set password.
+     *
+     * @param string $password
+     *
+     * @return User
+     */
+    public function setPassword($password)
+    {
+        $this->password = $password;
+
+        return $this;
+    }
+
+    /**
+     * Get password.
+     *
+     * @return string
+     */
+    public function getPassword()
+    {
+        return $this->password;
+    }
+
+    /**
+     * Set dateCreation.
+     *
+     * @param datetime_immutable $dateCreation
+     *
+     * @return User
+     */
+    public function setDateCreation($dateCreation)
+    {
+        $this->dateCreation = $dateCreation;
+
+        return $this;
+    }
+
+    /**
+     * Get dateCreation.
+     *
+     * @return datetime_immutable
+     */
+    public function getDateCreation()
+    {
+        return $this->dateCreation;
+    }
+
+    /**
+     * Set updatedAt.
+     *
+     * @param datetime_immutable|null $updatedAt
+     *
+     * @return User
+     */
+    public function setUpdatedAt($updatedAt = null)
+    {
+        $this->updatedAt = $updatedAt;
+
+        return $this;
+    }
+
+    /**
+     * Get updatedAt.
+     *
+     * @return datetime_immutable|null
+     */
+    public function getUpdatedAt()
+    {
+        return $this->updatedAt;
+    }
+
+    /**
+     * Set dateDeactivation.
+     *
+     * @param datetime_immutable|null $dateDeactivation
+     *
+     * @return User
+     */
+    public function setDateDeactivation($dateDeactivation = null)
+    {
+        $this->dateDeactivation = $dateDeactivation;
+
+        return $this;
+    }
+
+    /**
+     * Get dateDeactivation.
+     *
+     * @return datetime_immutable|null
+     */
+    public function getDateDeactivation()
+    {
+        return $this->dateDeactivation;
+    }
+}
