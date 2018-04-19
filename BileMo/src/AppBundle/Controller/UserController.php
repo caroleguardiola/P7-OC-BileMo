1fd8615a8c6f98f15b3f53f5441d54273a8d8595
caroleguardiola <carole.guardiola@gmail.com>
2018-04-19 17:56:59 +0200

    Created pagination of the users' list.

--- BileMo/src/AppBundle/Controller/UserController.php
+++ BileMo/src/AppBundle/Controller/UserController.php
@@ -8,9 +8,10 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\Representation\Users;
 use FOS\RestBundle\Controller\Annotations as Rest;
 use FOS\RestBundle\Controller\FOSRestController;
-
+use FOS\RestBundle\Request\ParamFetcherInterface;
 class UserController extends FOSRestController
 {
     /**
@@ -18,15 +19,43 @@ class UserController extends FOSRestController
      *     path = "/users",
      *     name = "app_users_list",
      * )
-     *
+     * @Rest\QueryParam(
+     *     name="keyword",
+     *     requirements="[a-zA-Z0-9\s]+",
+     *     nullable=true,
+     *     description="The keyword to search for."
+     * )
+     * @Rest\QueryParam(
+     *     name="order",
+     *     requirements="asc|desc",
+     *     default="asc",
+     *     description="Sort order (asc or desc)"
+     * )
+     * @Rest\QueryParam(
+     *     name="limit",
+     *     requirements="\d+",
+     *     default="15",
+     *     description="Max number of mobile phones per page."
+     * )
+     * @Rest\QueryParam(
+     *     name="offset",
+     *     requirements="\d+",
+     *     default="0",
+     *     description="The pagination offset"
+     * )
      *  @Rest\View(
      *     statusCode = 200
      * )
      */
-    public function listAction()
+    public function listAction(ParamFetcherInterface $paramFetcher)
     {
-        $users = $this->getDoctrine()->getRepository('AppBundle:User')->findAll();
+        $pager = $this->getDoctrine()->getRepository('AppBundle:User')->search(
+            $paramFetcher->get('keyword'),
+            $paramFetcher->get('order'),
+            $paramFetcher->get('limit'),
+            $paramFetcher->get('offset')
+        );
 
-        return $users;
+        return new Users($pager);
     }
 }
