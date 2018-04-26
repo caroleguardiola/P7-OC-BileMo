0f24979d1c3bcad94e287127168c5ecd553ccd9f
caroleguardiola <carole.guardiola@gmail.com>
2018-04-26 17:57:50 +0200

    Created AccessToken.

---
+++ BileMo/src/AppBundle/Entity/AccessToken.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Carole Guardiola
+ * Date: 26/04/2018
+ * Time: 17:53
+ */
+
+namespace AppBundle\Entity;
+
+use FOS\OAuthServerBundle\Entity\AccessToken as BaseAccessToken;
+use Doctrine\ORM\Mapping as ORM;
+
+class AccessToken extends BaseAccessToken
+{
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="AppBundle\Entity\Customer")
+     * @ORM\JoinColumn(nullable=false)
+     */
+    protected $customer;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="AppBundle\Entity\User")
+     * @ORM\JoinColumn(name="user_id", referencedColumnName="id", onDelete="CASCADE")
+     */
+    protected $user;
+}
