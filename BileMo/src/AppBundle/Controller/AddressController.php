0787cdb793fb3d0af18a5ab45a57b99f02bf8ade
caroleguardiola <carole.guardiola@gmail.com>
2018-05-05 14:43:25 +0200

    Added listAction in AddressController.

--- BileMo/src/AppBundle/Controller/AddressController.php
+++ BileMo/src/AppBundle/Controller/AddressController.php
@@ -9,14 +9,61 @@
 namespace AppBundle\Controller;
 
 use AppBundle\Entity\Address;
+use AppBundle\Representation\Addresses;
 use FOS\RestBundle\Controller\Annotations as Rest;
 use FOS\RestBundle\Controller\FOSRestController;
+use FOS\RestBundle\Request\ParamFetcherInterface;
 use AppBundle\Exception\ResourceNotFoundException;
 use AppBundle\Exception\ResourceAccessForbiddenException;
 
 
 class AddressController extends FOSRestController
 {
+    /**
+     * @param ParamFetcherInterface $paramFetcher
+     * @return Addresses
+     *
+     * @Rest\Get(
+     *     path = "/api/addresses",
+     *     name = "app_addresses_list",
+     * )
+     * @Rest\QueryParam(
+     *     name="order",
+     *     requirements="asc|desc",
+     *     default="asc",
+     *     description="Sort order (asc or desc)"
+     * )
+     * @Rest\QueryParam(
+     *     name="limit",
+     *     requirements="\d+",
+     *     default="15",
+     *     description="Max number of addresses phones per page."
+     * )
+     * @Rest\QueryParam(
+     *     name="offset",
+     *     requirements="\d+",
+     *     default="0",
+     *     description="The pagination offset"
+     * )
+     *  @Rest\View(
+     *     statusCode = 200,
+     *     serializerGroups = {"Default", "list_users"}
+     * )
+     */
+    public function listAction(ParamFetcherInterface $paramFetcher)
+    {
+        $customer = $this->getUser();
+
+        $pager = $this->getDoctrine()->getRepository('AppBundle:Address')->search(
+            $customer,
+            $paramFetcher->get('order'),
+            $paramFetcher->get('limit'),
+            $paramFetcher->get('offset')
+        );
+
+        return new Addresses($pager);
+    }
+
     /**
      * @param Address $address
      * @return Address
