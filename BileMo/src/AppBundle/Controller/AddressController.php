f493ddcb766968edeb1a284c69c3764fd9755289
caroleguardiola <carole.guardiola@gmail.com>
2018-05-08 12:10:25 +0200

    Added SWG annotations to AddressController.

--- BileMo/src/AppBundle/Controller/AddressController.php
+++ BileMo/src/AppBundle/Controller/AddressController.php
@@ -15,6 +15,9 @@ use FOS\RestBundle\Controller\FOSRestController;
 use FOS\RestBundle\Request\ParamFetcherInterface;
 use AppBundle\Exception\ResourceNotFoundException;
 use AppBundle\Exception\ResourceAccessForbiddenException;
+use Nelmio\ApiDocBundle\Annotation\Model;
+use Nelmio\ApiDocBundle\Annotation\Security;
+use Swagger\Annotations as SWG;
 
 
 class AddressController extends FOSRestController
@@ -45,9 +48,35 @@ class AddressController extends FOSRestController
      *     default="0",
      *     description="The pagination offset"
      * )
-     *  @Rest\View(
+     * @Rest\View(
      *     statusCode = 200,
      *     serializerGroups = {"Default", "list_users", "list_addresses"}
+     * )
+     * @SWG\Get(
+     *   path="/api/addresses",
+     *   tags={"Addresses"},
+     *   summary="Get the list of all addresses of the users",
+     *   description="To access to this resource, you need to enter in the authorization: Bearer 'YourAccessToken'",
+     *   operationId="getAddressById",
+     *   produces={"application/json"},
+     *   @SWG\Parameter(
+     *     name="Authorization",
+     *     in="header",
+     *     type="string",
+     *     description="Bearer 'YourAccessToken' ",
+     *     required=true,
+     *   ),
+     *   @SWG\Response(
+     *     response=200,
+     *     description="Successful operation",
+     *     @Model(type=Address::class, groups={"list_users", "list_addresses"})
+     *   ),
+     *   @SWG\Response(
+     *     response=403,
+     *     description="No permission to access at this resource"),
+     *   @SWG\Response(
+     *     response=404,
+     *     description="Resource not found")
      * )
      */
     public function listAction(ParamFetcherInterface $paramFetcher)
@@ -80,17 +109,45 @@ class AddressController extends FOSRestController
      *     statusCode = 200,
      *     serializerGroups = {"detail_address"}
      * )
+     * @SWG\Get(
+     *   path="/api/addresses/{id}",
+     *   tags={"Addresses"},
+     *   summary="Get the detail of an user's address by ID",
+     *   description="To access to this resource, you need to enter :
+            - in the authorization: Bearer 'YourAccessToken'
+            - in the path: a valid ID",
+     *   operationId="getAddressById",
+     *   produces={"application/json"},
+     *   @SWG\Parameter(
+     *     name="Authorization",
+     *     in="header",
+     *     type="string",
+     *     description="Bearer 'YourAccessToken' ",
+     *     required=true,
+     *   ),
+     *   @SWG\Response(
+     *     response=200,
+     *     description="Successful operation",
+     *     @Model(type=Address::class, groups={"detail_address"})
+     *   ),
+     *   @SWG\Response(
+     *     response=403,
+     *     description="No permission to access at this resource"),
+     *   @SWG\Response(
+     *     response=404,
+     *     description="Resource not found")
+     * )
      */
     public function showAction(Address $address=null)
     {
         $customer = $this->getUser();
 
-        if (empty($address)){
-            throw new ResourceNotFoundException('This resource doesn\'t exist.');
-        }
-
         if($customer !== $address->getUser()->getCustomer()){
             throw new ResourceAccessForbiddenException('You don\'t have the permission to access to this resource.');
+        }
+
+        if (empty($address)){
+            throw new ResourceNotFoundException('This resource doesn\'t exist.');
         }
 
         return $address;
